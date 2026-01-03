3d535f17021b5854e9fd14c3dca24b83e5553049
edobasky <edore7@gmail.com>
2026-01-03 10:44:35 +0100

    mm

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,45 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { EmployeeForm } from './pages/employee-form/employee-form';
+import { Department } from './pages/department/department';
+import { Designation } from './pages/designation/designation';
+import { EmployeeList } from './pages/employee-list/employee-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: '',
+    component: Headers,
+    children: [
+      {
+        path: 'dashboard',
+        component: Dashboard,
+      },
+      {
+        path: 'new-employee',
+        component: EmployeeForm,
+      },
+      {
+        path: 'employee-list',
+        component: EmployeeList,
+      },
+      {
+        path: 'department',
+        component: Department,
+      },
+      {
+        path: 'designation',
+        component: Designation,
+      },
+    ],
+  },
+];
